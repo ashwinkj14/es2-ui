01a00d9a049eaaf18cc86a7039426fb911ddf805
ashwinkj14 <ashwinkj97@gmail.com>
2023-09-21 21:06:47 -0400

    Use routes for loading pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Login from './components/login'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
