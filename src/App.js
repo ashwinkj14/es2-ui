8b32a7b116ee84d583332af1ac7812d9b568c4a8
ashwinkj14 <ashwinkj97@gmail.com>
2023-09-25 12:21:04 -0400

    Add route for publication page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Login from './components/login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Publication from './components/publication';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
+        <Route path="/publication" element={<Publication/>}></Route>
       </Routes>
     </BrowserRouter>
   );
