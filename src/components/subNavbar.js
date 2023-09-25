e1eedce8f21380faa5be777c97a5248e0ac3da4e
ashwinkj14 <ashwinkj97@gmail.com>
2023-09-25 12:18:12 -0400

    Create sub navbar component

---
+++ src/components/subNavbar.js
@@ -0,0 +1,20 @@
+import '../styles/subNavbar.css';
+function SubNavbar(){
+    return(
+        <div>
+            <section className="sideNav-container">
+                <section className="sideNav-elements-container">
+                    <section className="sideNav-elements">
+                        <div className="sideNav-element">Search</div>
+                        <div className="sideNav-element">Manage</div>
+                    </section>
+                    <section className="add-pub-btn-container">
+                        <div className="add-pub-btn">Add Publication</div>
+                    </section>
+                </section>
+            </section>
+        </div>
+    );
+}
+
+export default SubNavbar;
