aa9465fc2e901a94b5d82cba0eea119c3cb23699
ashwinkj14 <ashwinkj97@gmail.com>
2023-10-15 18:24:04 -0400

    Add Database component

---
+++ src/components/Database/DatabaseSettings.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable require-jsdoc */
+function DatabaseSettings() {
+  return (
+    <div></div>
+  );
+}
+
+export default DatabaseSettings;
