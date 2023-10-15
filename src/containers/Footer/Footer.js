0fed1a8b9c032f1c0e7120cb9838f0b1182b4ae5
ashwinkj14 <ashwinkj97@gmail.com>
2023-10-15 18:19:54 -0400

    Add footer container

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,13 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable react/react-in-jsx-scope */
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+
+    </div>
+  );
+}
+
+export default Footer;
