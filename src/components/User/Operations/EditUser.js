63b52cc3e34280993db91e071809bbc7a9ad361f
ashwinkj14 <ashwinkj97@gmail.com>
2023-11-07 19:19:58 -0500

    Add validation to input fields in edit user page

--- src/components/User/Operations/EditUser.js
+++ src/components/User/Operations/EditUser.js
@@ -22,7 +22,46 @@ function EditUser({action, user}) {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const [usernameError, setUsernameError] = useState('');
+  const [emailIdError, setEmailIdError] = useState('');
+  const [organizationError, setOrganizationError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
+  const displayError = (Error) => {
+    if (Error=='') {
+      return (<></>);
+    }
+    return (
+      <section className='add-user-error-container'>{Error}</section>
+    );
+  };
+
+  const handleValidation = () => {
+    let errorExists = false;
+    if (!username) {
+      errorExists = true;
+      setUsernameError('Username is required');
+    }
+    if (!emailId) {
+      errorExists = true;
+      setEmailIdError('Email ID is required');
+    }
+    if (!organization) {
+      errorExists = true;
+      setOrganizationError('Organization is required');
+    }
+    if (password!=confirmPassword) {
+      setConfirmPasswordError('Password doesn\'t match');
+      setPasswordError('Password doesn\'t match');
+    }
+    return errorExists;
+  };
+
   const handleUpdate = () => {
+    if (handleValidation()) {
+      return;
+    }
     const api = `http://localhost:8080/user/update`;
 
     const data = {
@@ -61,25 +100,40 @@ function EditUser({action, user}) {
             <option value="4">Student</option>
           </select>
         </div>
-        <div className='add-user-text-field-container'>
-          <label>User Name</label>
-          <input value={username} onChange={(event) => setUsername(event.target.value)} className="text-field" type="text"/>
+        <div className='add-user-field-container'>
+          <div className='add-user-text-field-container'>
+            <label>User Name</label>
+            <input value={username} onChange={(event) => setUsername(event.target.value)} className="text-field" type="text"/>
+          </div>
+          {displayError(usernameError)}
         </div>
-        <div className='add-user-text-field-container'>
-          <label>Email ID</label>
-          <input value={emailId} onChange={(event) => setEmailId(event.target.value)} className="text-field" type="text"/>
+        <div className='add-user-field-container'>
+          <div className='add-user-text-field-container'>
+            <label>Email ID</label>
+            <input value={emailId} onChange={(event) => setEmailId(event.target.value)} className="text-field" type="text"/>
+          </div>
+          {displayError(emailIdError)}
         </div>
-        <div className='add-user-text-field-container'>
-          <label>Organization</label>
-          <input value={organization} onChange={(event) => setOrganization(event.target.value)} className="text-field" type="text"/>
+        <div className='add-user-field-container'>
+          <div className='add-user-text-field-container'>
+            <label>Organization</label>
+            <input value={organization} onChange={(event) => setOrganization(event.target.value)} className="text-field" type="text"/>
+          </div>
+          {displayError(organizationError)}
         </div>
-        <div className='add-user-text-field-container'>
-          <label>Password</label>
-          <input value={password} onChange={(event) => setPassword(event.target.value)} className="text-field" type="password"/>
+        <div className='add-user-field-container'>
+          <div className='add-user-text-field-container'>
+            <label>Password</label>
+            <input value={password} onChange={(event) => setPassword(event.target.value)} className="text-field" type="password"/>
+          </div>
+          {displayError(passwordError)}
         </div>
-        <div className='add-user-text-field-container'>
-          <label>Confirm Password</label>
-          <input value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} className="text-field" type="password"/>
+        <div className='add-user-field-container'>
+          <div className='add-user-text-field-container'>
+            <label>Confirm Password</label>
+            <input value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} className="text-field" type="password"/>
+          </div>
+          {displayError(confirmPasswordError)}
         </div>
       </div>
       <div className="add-user-submit-btn-container">
