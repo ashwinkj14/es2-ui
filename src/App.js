7a6549d257a60e861b875272bc61b8db6b05d4d2
ashwinkj14 <ashwinkj97@gmail.com>
2023-12-06 09:28:23 -0500

    Resolve continuous refresh issue and change application title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/react-in-jsx-scope */
-import {useEffect} from 'react';
-import {BrowserRouter, Routes, Route, Navigate, useNavigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './components/Login/Login';
 import Publication from './components/Publication/Publication';
 import Patent from './components/Patent/Patent';
@@ -12,22 +11,6 @@ import NotFound from './components/404/NotFound';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    window.addEventListener('beforeunload', (event) => {
-      event.preventDefault();
-      event.returnValue = '';
-      navigate(0);
-    });
-
-    return () => {
-      window.removeEventListener('beforeunload', (event) => {
-        event.preventDefault();
-        event.returnValue = '';
-      });
-    };
-  }, []);
-
   return (
     <div>
       <BrowserRouter>
