005f6e03cceb9a7cf8a95dacf782f466bbe70a48
ashwinkj14 <ashwinkj97@gmail.com>
2024-01-03 21:38:02 -0500

    Feature: Change the text fields to material UI components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,17 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable require-jsdoc */
 import axios from 'axios';
+import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useUserStore} from '../../store/es2Store';
 import {FAILURE, displayToast} from '../ToastUtil';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import MenuItem from '@mui/material/MenuItem';
 
 import {BASE_URL} from '../../server-constants';
 import logo from '../../es2-logo-final.jpg';
@@ -13,28 +21,47 @@ import './Login.css';
 import {useEffect} from 'react';
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userType, setUserType] = useState('');
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const userTypeOptions = [
+    {
+      value: 'admin',
+      label: 'admin',
+    },
+    {
+      value: 'member company',
+      label: 'member company',
+    },
+    {
+      value: 'student',
+      label: 'student',
+    },
+    {
+      value: 'faculty',
+      label: 'faculty',
+    },
+  ];
+
   const navigate = useNavigate();
   const setUserTypeId = useUserStore((state) => state.setUserTypeId);
   const doLogin = () => {
-    const usernameElement = document.getElementById('login-username');
-    const username = usernameElement.value;
     if (username === '') {
       alert('username is required');
-      usernameElement.focus();
       return false;
     }
-    const passwordElement = document.getElementById('login-password');
-    const password = passwordElement.value;
     if (password === '') {
       alert('password is required');
-      passwordElement.focus();
       return false;
     }
-    const userTypeElement = document.getElementById('login-usertype');
-    const userType = userTypeElement.value;
     if (userType === '') {
       alert('usertype is required');
-      userTypeElement.focus();
       return false;
     }
 
@@ -118,18 +145,105 @@ function Login() {
         <div className='login-container'>
           <div className='login-header'>ES2 Repository</div>
           <section className='login-input-container'>
-            <input id="login-username" className="login-input"
-              placeholder='username'></input>
-            <input id="login-password" className="login-input" type='password'
-              placeholder='password'></input>
-            <select id="login-usertype" className="login-input"
-              required name="usertype">
-              <option value="">usertype</option>
-              <option value="admin">admin</option>
-              <option value="member company">member company</option>
-              <option value="student">student</option>
-              <option value="faculty">faculty</option>
-            </select>
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': {m: 1, width: '32ch'},
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-username"
+                label="Username"
+                defaultValue=""
+                className="login-input"
+                size="small"
+                color='success'
+                sx={{
+                  mb: 1,
+                  width: '30ch',
+                  fontSize: '0.9rem',
+                  fontWeight: 300,
+                }}
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                InputLabelProps={{
+                  sx: {
+                    'fontSize': '0.9rem',
+                    'fontWeight': 400,
+                    '&.MuiOutlinedInput-notchedOutline': {fontSize: '0.9rem'},
+                  },
+                }}
+              ></TextField>
+              <TextField
+                id="outlined-password"
+                label="Password"
+                defaultValue=""
+                className="login-input"
+                size="small"
+                type={showPassword ? 'text' : 'password'}
+                sx={{
+                  mb: 1,
+                  width: '30ch',
+                  fontSize: '0.9rem',
+                  fontWeight: 300,
+                }}
+                color='success'
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                InputLabelProps={{
+                  sx: {
+                    'fontSize': '0.9rem',
+                    'fontWeight': 400,
+                    '&.MuiOutlinedInput-notchedOutline': {fontSize: '0.9rem'},
+                  },
+                }}
+                InputProps={{
+                  endAdornment:
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>,
+                }}
+              >
+              </TextField>
+              <TextField
+                id="outlined-usertype-select"
+                select
+                label="User Type"
+                defaultValue=""
+                className="login-input"
+                size="small"
+                color='success'
+                onChange={(event) => setUserType(event.target.value)}
+                sx={{
+                  mb: 1,
+                  width: '30ch',
+                  fontSize: '0.9rem',
+                  fontWeight: 300,
+                }}
+                InputLabelProps={{
+                  sx: {
+                    'fontSize': '0.9rem',
+                    'fontWeight': 400,
+                    '&.MuiOutlinedInput-notchedOutline': {fontSize: '0.9rem'},
+                  },
+                }}
+              >
+                {userTypeOptions.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Box>
             <section className='login-btn-container'>
               <button className='login-btn' onClick={() => doLogin()}>Login</button>
             </section>
