cdd97c0efc724d2a7af1c7fb6067f86be835e5cf
ashwinkj14 <ashwinkj97@gmail.com>
2024-08-10 20:15:07 -0400

    Refactor: Minor UI Changes, add translate to modal

--- src/components/Project/CustomSideBar/CustomSideBar.js
+++ src/components/Project/CustomSideBar/CustomSideBar.js
@@ -1,14 +1,20 @@
+/* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/react-in-jsx-scope */
+import {useState} from 'react';
 import './CustomSideBar.css';
 
 function CustomSideBar({content, closeAction}) {
+  const [displayer, setDisplayer] = useState('popup-displayer popup-open-animation');
   const handleClose = () =>{
-    closeAction();
+    setDisplayer('popup-displayer popup-close-animation');
+    setTimeout(()=>{
+      closeAction();
+    }, 300);
   };
   return (
-    <div className="popup-displayer">
+    <div className={displayer}>
       <section className='popup-close' onClick={handleClose}>X</section>
       <section className="custom-popup-body">
         {content}
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,15 +1,21 @@
+/* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/react-in-jsx-scope */
 import './SideBar.css';
+import {useState} from 'react';
 
 function SideBar({content, closeAction}) {
+  const [displayer, setDisplayer] = useState('popup-displayer popup-open-animation');
   const handleClose = () =>{
-    closeAction();
+    setDisplayer('popup-displayer popup-close-animation');
+    setTimeout(()=>{
+      closeAction();
+    }, 300);
   };
   return (
-    <div className="popup-displayer">
-      <section className='popup-close' onClick={handleClose}>X</section>
+    <div className={displayer}>
+      <section className='popup-close ' onClick={handleClose}>X</section>
       <section className="popup-body">
         {content}
       </section>
